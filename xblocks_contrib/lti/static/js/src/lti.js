3aa8158e2f25af6324b049e7a795d306fe78ded6
Muhammad Tayayb Tahir Qureshi <tayyab.tahir@arbisoft.com>
2024-10-21 16:49:23 +0500

    feat: Extracting LTI xblock from edx-platform

--- xblocks_contrib/lti/static/js/src/lti.js
+++ xblocks_contrib/lti/static/js/src/lti.js
@@ -1,38 +1,36 @@
-
 /* JavaScript for LTIBlock. */
-function LTIBlock(runtime, element) {
-    const updateCount = (result) => {
-        $('.count', element).text(result.count);
-    };
 
-    const handlerUrl = runtime.handlerUrl(element, 'increment_count');
+(function() {
+    'use strict';
 
-    $('p', element).on('click', (eventObject) => {
-        $.ajax({
-            type: 'POST',
-            url: handlerUrl,
-            contentType: 'application/json',
-            data: JSON.stringify({hello: 'world'}),
-            success: updateCount
-        });
-    });
+    /**
+     * This function will process all the attributes from the DOM element passed, taking all of
+     * the configuration attributes. It uses the request-username and request-email
+     * to prompt the user to decide if they want to share their personal information
+     * with the third party application connecting through LTI.
+     * @constructor
+     * @param {jQuery} element DOM element with the lti container.
+     */
+    this.LTI = function(element) {
+        var dataAttrs = $(element).find('.lti').data(),
+            askToSendUsername = (dataAttrs.askToSendUsername === 'True'),
+            askToSendEmail = (dataAttrs.askToSendEmail === 'True');
 
-    $(() => {
-        /*
-        Use `gettext` provided by django-statici18n for static translations
-        */
-
-        // eslint-disable-next-line no-undef
-        const dummyText = gettext('Hello World');
-
-        // Example usage of interpolation for translated strings
-        // eslint-disable-next-line no-undef
-        const message = StringUtils.interpolate(
-            gettext('You are enrolling in {courseName}'),
-            {
-                courseName: 'Rock & Roll 101'
+        // When the lti button is clicked, provide users the option to
+        // accept or reject sending their information to a third party
+        $(element).on('click', '.link_lti_new_window', function() {
+            if (askToSendUsername && askToSendEmail) {
+                // eslint-disable-next-line no-alert
+                return confirm(gettext('Click OK to have your username and e-mail address sent to a 3rd party application.\n\nClick Cancel to return to this page without sending your information.'));
+            } else if (askToSendUsername) {
+                // eslint-disable-next-line no-alert
+                return confirm(gettext('Click OK to have your username sent to a 3rd party application.\n\nClick Cancel to return to this page without sending your information.'));
+            } else if (askToSendEmail) {
+                // eslint-disable-next-line no-alert
+                return confirm(gettext('Click OK to have your e-mail address sent to a 3rd party application.\n\nClick Cancel to return to this page without sending your information.'));
+            } else {
+                return true;
             }
-        );
-        console.log(message); // This is just for demonstration purposes
-    });
-}
+        });
+    };
+}).call(this);
